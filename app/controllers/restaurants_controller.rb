867792676499f5c2453ba07e768a790338f6d60a
Brandao Plaster <brandaoplaster@gmail.com>
2019-10-25 15:23:18 -0300

    implement index and filter_by category methods in the restaurant controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,22 @@
 class RestaurantsController < ApplicationController
+
   def index
+    @restaurants = Restaurant.near(params[:city] || 'São Paulo')
+    filter_by_category if params[:category]
+    render json: @restaurants
   end
 
   def show
   end
 
   def search
+  end
+
+  private
+
+  def filter_by_category
+    @restaurants = @restaurants.select do |r|
+      r.category.title == params[:category]
+    end
   end
 end
