568f829c4b463f4dc28d1a8cfedaf153135971fe
Brandao Plaster <brandaoplaster@gmail.com>
2019-10-25 11:45:32 -0300

    Active storage installed. Updated rails to version 5.2. Removed config/initializers/new_framework_defaults.rb. Generated a migrate.

--- db/schema.rb
+++ db/schema.rb
@@ -10,87 +10,109 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191024171945) do
+ActiveRecord::Schema.define(version: 2019_10_25_143938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string   "title"
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.bigint "byte_size", null: false
+    t.string "checksum", null: false
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "order_products", force: :cascade do |t|
-    t.integer  "quantity"
-    t.text     "comment"
-    t.integer  "order_id"
-    t.integer  "product_id"
+  create_table "order_products", id: :serial, force: :cascade do |t|
+    t.integer "quantity"
+    t.text "comment"
+    t.integer "order_id"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["order_id"], name: "index_order_products_on_order_id", using: :btree
-    t.index ["product_id"], name: "index_order_products_on_product_id", using: :btree
+    t.index ["order_id"], name: "index_order_products_on_order_id"
+    t.index ["product_id"], name: "index_order_products_on_product_id"
   end
 
-  create_table "orders", force: :cascade do |t|
-    t.string   "name"
-    t.string   "phone_number"
-    t.float    "total_value"
-    t.string   "address"
-    t.integer  "status",        default: 0
-    t.integer  "restaurant_id"
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.index ["restaurant_id"], name: "index_orders_on_restaurant_id", using: :btree
+  create_table "orders", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.string "phone_number"
+    t.float "total_value"
+    t.string "address"
+    t.integer "status", default: 0
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_orders_on_restaurant_id"
   end
 
-  create_table "product_categories", force: :cascade do |t|
-    t.string   "title"
-    t.integer  "restaurant_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["restaurant_id"], name: "index_product_categories_on_restaurant_id", using: :btree
+  create_table "product_categories", id: :serial, force: :cascade do |t|
+    t.string "title"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_product_categories_on_restaurant_id"
   end
 
-  create_table "products", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.float    "price"
-    t.integer  "product_category_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.index ["product_category_id"], name: "index_products_on_product_category_id", using: :btree
+  create_table "products", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
+    t.integer "product_category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_category_id"], name: "index_products_on_product_category_id"
   end
 
-  create_table "restaurants", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.integer  "status"
-    t.float    "delivery_tax"
-    t.string   "state"
-    t.string   "city"
-    t.string   "street"
-    t.string   "neighborhood"
-    t.string   "number"
-    t.string   "complement"
-    t.string   "reference"
-    t.string   "cep"
-    t.float    "latitude"
-    t.float    "longitude"
-    t.integer  "category_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
-    t.index ["category_id"], name: "index_restaurants_on_category_id", using: :btree
+  create_table "restaurants", id: :serial, force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "status"
+    t.float "delivery_tax"
+    t.string "state"
+    t.string "city"
+    t.string "street"
+    t.string "neighborhood"
+    t.string "number"
+    t.string "complement"
+    t.string "reference"
+    t.string "cep"
+    t.float "latitude"
+    t.float "longitude"
+    t.integer "category_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_restaurants_on_category_id"
   end
 
-  create_table "reviews", force: :cascade do |t|
-    t.integer  "value"
-    t.integer  "restaurant_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id", using: :btree
+  create_table "reviews", id: :serial, force: :cascade do |t|
+    t.integer "value"
+    t.integer "restaurant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_reviews_on_restaurant_id"
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "order_products", "orders"
   add_foreign_key "order_products", "products"
   add_foreign_key "orders", "restaurants"
