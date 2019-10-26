d716891e53714e5dadf9ec7927129531c73c05f3
Brandao Plaster <brandaoplaster@gmail.com>
2019-10-26 16:12:21 -0300

    Implemetation of set price method in model order as before validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,4 +7,18 @@ class Order < ApplicationRecord
   validates :total_value, presence: true
 
   enum status: { waiting: 0, delivered: 1 }
+
+  before_validation :set_price
+
+  private
+
+  def set_price
+    @final_price = 0
+    order_products.each do |order_product|
+      product = Product.find order_product.product_id
+      @final_price += order_product.quantity * product.price
+    end
+    self.total_value = @final_price
+  end
+
 end
