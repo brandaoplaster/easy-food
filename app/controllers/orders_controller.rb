430574e4e7361446c203433af539d9c5ccad59e5
Brandao Plaster <brandaoplaster@gmail.com>
2019-10-26 16:39:32 -0300

    Implemented create method

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,15 @@ class OrdersController < ApplicationController
 
   before_action :set_order, only: :show
 
+  def create
+    order = Order.new(order_params)
+    if order.save
+      render json: @order, status: :created
+    else
+      render json: order.errors, status: :unprocessable_entity
+    end
+  end
+
   def show
     render json: @order
   end
