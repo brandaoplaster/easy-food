0e0702c093460860823a43fc3dede512a142a95e
Brandao Plaster <brandaoplaster@gmail.com>
2019-10-26 16:22:38 -0300

    Created private methods set_order and order_params

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,17 @@
 class OrdersController < ApplicationController
   def show
   end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  end
+
+  def order_params
+    params.require(:order).permit(:name, :phone_number, :restaurant_id,
+                                  order_products_attributes: %i[quantity comment product_id]
+    )
+  end
+
 end
